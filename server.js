b9905d7a5929a470b8cb6a4bc8826d075a5dcddc
wymeric <wymeric89@gmail.com>
2017-08-10 23:17:15 +0800

    Signed-off-by: wymeric <wymeric89@gmail.com>

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ var mongoose = require('mongoose');
     passport = require('passport');
     routes = require('./routes/routes');
     bodyParser = require('body-parser');
+
+var allowAcrossDomain = function(req,res,next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+    // if ('OPTIONS' == req.method) {
+    //     res.send(200);
+    // }
+    // else {
+    //     next();
+    // }
+}
     
 
 mongoose.connect(config.database);
@@ -13,6 +27,7 @@ mongoose.connect(config.database);
 mongoose.connection.on('open', function(){
     console.log('Mongo is connected');
     var app = express();
+    app.use(allowAcrossDomain);
     app.use(morgan('dev'));
     app.use(cors());
     app.use(bodyParser.urlencoded({extended: false}));
