c80c3e9839335f038ccd7d8c1d9b5eb62b347c79
Eric Wong <wymeric89@gmail.com>
2017-08-16 20:41:04 +0800

    Signed-off-by: Eric Wong <wymeric89@gmail.com>

--- npm-install.cmd
+++ npm-install.cmd
@@ -2,5 +2,7 @@ npm install mongoose express morgan cors
 npm install bcrypt
 npm install passport passport-jwt jwt-simple
 npm install passport passport-oauth2
+npm install passport passport-google-oauth2
 npm install body-parser
+npm install mssql
 
